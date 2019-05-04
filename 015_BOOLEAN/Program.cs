02cee032938407f573f39d6f0c002bd5dcead23d
Віктор Шереш <sheregi1000@gmail.com>
2019-05-04 19:46:41 +0300

    Comment

--- 015_BOOLEAN/Program.cs
+++ 015_BOOLEAN/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _014_BOOLEAN
+namespace _015_BOOLEAN
 {
     class Program
     {
@@ -15,11 +15,11 @@ namespace _014_BOOLEAN
             int number_B = Convert.ToInt32(Console.ReadLine());
             int number_С = Convert.ToInt32(Console.ReadLine());
 
-            bool A = number_A > 0;
-            bool B = number_B > 0;
-            bool C = number_С > 0;
+            bool A = number_A > 0 && number_B > 0 && number_С <= 0;
+            bool B = number_B > 0 && number_С > 0 && number_A <= 0;
+            bool C = number_С > 0 && number_A > 0 && number_B <= 0;
 
-            bool trueOrFalse = A ^ B ^ C;
+            bool trueOrFalse = A || B || C;
             Console.WriteLine("Число А={0} - {1}, число В={2} - {3}," +
                 "число C={4} - {5}, общий результат - {6}",
                 number_A, A, number_B, B, number_С, C, trueOrFalse);
